8b94d8c26c9091cf21f26fb74f0d10955882d0f5
menachemdesign.tech@gmail.com <menachemdesign.tech@gmail.com>
2024-05-12 01:44:51 -0400

    H

--- Portfolio/Gdocs/Server/server.js
+++ Portfolio/Gdocs/Server/server.js
@@ -1,42 +1,58 @@
-const mongoose = require("mongoose")
-const Document = require("./Document")
+const mongoose = require("mongoose");
+const Document = require("./Document");
 
 mongoose.connect("mongodb://localhost/google-docs-clone", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true,
-})
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch(err => {
+  console.error("Could not connect to MongoDB:", err);
+});
 
 const io = require("socket.io")(3001, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
-})
+});
 
-const defaultValue = ""
+const defaultValue = "";
 
 io.on("connection", socket => {
+  console.log("Connected:", socket.id);
+
   socket.on("get-document", async documentId => {
-    const document = await findOrCreateDocument(documentId)
-    socket.join(documentId)
-    socket.emit("load-document", document.data)
+    if (!documentId) return;
+    
+    try {
+      const document = await findOrCreateDocument(documentId);
+      socket.join(documentId);
+      socket.emit("load-document", document.data);
 
-    socket.on("send-changes", delta => {
-      socket.broadcast.to(documentId).emit("receive-changes", delta)
-    })
+      socket.on("send-changes", delta => {
+        socket.broadcast.to(documentId).emit("receive-changes", delta);
+      });
 
-    socket.on("save-document", async data => {
-      await Document.findByIdAndUpdate(documentId, { data })
-    })
-  })
-})
+      socket.on("save-document", async data => {
+        await Document.findByIdAndUpdate(documentId, { data });
+      });
+    } catch (error) {
+      console.error("Error handling get-document event:", error);
+      socket.emit("error", "Failed to load or create the document.");
+    }
+  });
+});
 
 async function findOrCreateDocument(id) {
-  if (id == null) return
-
-  const document = await Document.findById(id)
-  if (document) return document
-  return await Document.create({ _id: id, data: defaultValue })
+  try {
+    const document = await Document.findById(id);
+    if (document) return document;
+    return await Document.create({ _id: id, data: defaultValue });
+  } catch (error) {
+    console.error("Error finding or creating a document:", error);
+    throw error;  // Re-throw to handle in the calling function
+  }
 }
